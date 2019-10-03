b60f5a94cbb0abf496fde09d17b462ea33e1ec0b
Manpreet Kaur <>
2019-10-03 14:54:17 -0400

    ADDED CAMERA FUNCTION TO THE APP WITH THE ALERT DIALOG BUILDER

---
+++ Will_Project_Week_4/app/src/main/java/com/example/myapplicationtest/DatabaseHelper.java
@@ -0,0 +1,76 @@
+package com.example.myapplicationtest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "PerfectRideDB";
+    public static final String TABLE_NAME = "User_info";
+    public static final String COL_1 = "ID";
+    public static final String COL_2= "FirstName";
+    public static final String COL_3 = "LastName";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,FirstName TEXT,LastName TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String firstname,String lastname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,firstname);
+        contentValues.put(COL_3,lastname);
+
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String firstname,String lastname,String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,firstname);
+        contentValues.put(COL_3,lastname);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Will_Project_Week_4/app/src/main/java/com/example/myapplicationtest/EventListener.java
@@ -0,0 +1,7 @@
+package com.example.myapplicationtest;
+
+public interface EventListener {
+
+    public void sendDataToActivity(String data);
+
+}
